76ff071b27ef843be8efefe987863a2a7ba42b08
Javier Lobato <davidlobato99@gmail.com>
2024-12-17 18:38:53 -0500

    Firts Commit - And DB postgres configuration

---
+++ src/main/java/com/electronicbillingmvp/ElectronicBillingMvpApplication.java
@@ -0,0 +1,13 @@
+package com.electronicbillingmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicBillingMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicBillingMvpApplication.class, args);
+    }
+
+}
