b3887937a4b648b37cb2042e5226cdbfefb1d8fd
Javier Lobato <davidlobato99@gmail.com>
2024-12-19 14:45:14 -0500

    Refactoring Class Client

--- src/main/java/com/electronicbillingmvp/domain/Client.java
+++ src/main/java/com/electronicbillingmvp/domain/Client.java
@@ -7,58 +7,95 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String address;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
     private String documentType;
+
+    @Column(nullable = false, unique = true)
     private String documentNumber;
+
+    @Column
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
     private String email;
-    private String phone;
+
+
+    protected Client() {
+    }
+
+    public Client(String name,
+                  String documentType,
+                  String documentNumber,
+                  String phoneNumber,
+                  String address,
+                  String email) {
+
+        this.name = name;
+        this.documentType = documentType;
+        this.documentNumber = documentNumber;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.email = email;
+
+    }
 
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
+    public String getName() {
+        return name;
     }
 
     public String getDocumentType() {
         return documentType;
     }
 
-    public void setDocumentType(String documentType) {
-        this.documentType = documentType;
-    }
-
     public String getDocumentNumber() {
         return documentNumber;
     }
 
-    public void setDocumentNumber(String documentNumber) {
-        this.documentNumber = documentNumber;
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber != null && !phoneNumber.matches("\\d+")) {
+            throw new IllegalArgumentException("The phone number must contain only digits");
+        }
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public void setEmail(String email) {
+        if (!email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            throw new IllegalArgumentException("\"Invalid email format\"");
+        }
         this.email = email;
     }
 
-    public String getPhone() {
-        return phone;
-    }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
